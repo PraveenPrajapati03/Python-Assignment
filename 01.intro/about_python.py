c7ecad59e8d6a11003afce711378f32b4add67cb
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-08-22 14:11:39 +0530

    python 01 assignment

---
+++ 01.intro/about_python.py
@@ -0,0 +1,9 @@
+#This is my first python program and i am currentl learinig pyton language
+print("Print Hello") 
+print("My name is praveen kumar")
+
+#calculater
+A=float(input("enter first numbber:"))
+B=float(input("enter second number:"))
+x=A*B
+print(x)
---
+++ 01.intro/welcome.py
@@ -0,0 +1,9 @@
+#This is my first python program
+print("Print Hello") 
+print("My name is praveen kumar")
+
+#calculater
+A=int(input("enter first numbber:"))
+B=int(input("enter second number:"))
+x=A+B
+print(x)
