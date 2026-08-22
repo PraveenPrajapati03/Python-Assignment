a9987735ee7ddeaaaad3318218a3ce4e08caf94e
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-08-22 16:17:44 +0530

    02.variable file add

---
+++ 02.variable/code.py
@@ -0,0 +1,29 @@
+#Q=24
+student_name="Praveen Prajapati"
+Student_age="18"
+student_city="Nagaur Rajasthan"
+print(student_name,Student_age,student_city)
+#Q=25
+student_name="Praveen Prajapati"
+Student_roll_number="112513"
+student_branch="B.tech CSE"
+print(student_name,Student_roll_number,student_branch)
+#Q=26
+marks=200
+marks=2000
+print(marks)
+#Q=27
+name,age,city="rahul",18,"Patna"
+print(name,age,city)
+#Q=28
+x=y=z=0
+print(x,y,z)
+#Q=29
+student1="Rahul"
+Student_name="Rahul"
+Class= "B.tech"
+#Q=30
+student_name="Praveen Prajapati"
+stuudent_age=17
+student_age=18
+print(student_name,student_age)
