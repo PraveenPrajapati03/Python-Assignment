6619f8d43e68be002661f0cf17854372b6e428fd
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-08-26 17:38:31 +0530

    new file added

---
+++ 05.string_operations/code.py
@@ -0,0 +1,15 @@
+#Part 3 — String Creation and Basic Operations
+#Task 1 — Create Strings
+your_name='Praveen Prajapati'
+your_city='Nagaur'
+favorite_language="Python"
+message="my name is praveen prajapati form nagaur rajasthan and i am btech cse student at coding gita"
+print("Output for task 1:")
+print(your_name,your_city,favorite_language,message)
+
+#Task 2 — Empty String
+a=""
+print("Output for task 2:")
+print(a,len(a),type(a))
+
+#Task 3 — String Information
