968f20d15adf81ed021f6ca65c3efe7d1df84515
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-09-04 18:30:27 +0530

    assignment uploaded

---
+++ 07.input and output/code.py
@@ -0,0 +1,175 @@
+#Practice Problems
+#1
+print("Output for Question 1:.................................................")
+name=input("Enter your name:")
+print("your name is:",name)
+
+#2
+print("Output for Question 2:.................................................")
+city=input("Enter your city:")
+print("Your city is",city)
+
+#3
+print("Output for Question 3:.................................................")
+user_name=input("Enter your namr:")
+user_age=int(input("Enter your age"))
+print(user_name)
+print(user_age)
+
+#4
+print("Output for Question 4:.................................................")
+print(type(input()))
+
+#5
+print("Output for Question 5:.................................................")
+a=input("Enter your name:")
+print(type(a))
+
+#6
+print("Output for Question 6:.................................................")
+fname=input("Enter your first name:")
+lname=input("Enter your last name:")
+print("your full name is:",fname+lname)
+
+#7
+print("Output for Question 7:.................................................")
+name=input("Enter your name:")
+city=input("Enter your city:")
+college=input("Enter your college:")
+print(name)
+print(city)
+print(college)
+
+#8
+print("Output for Question 8:.................................................")
+fname,lname=input("Enter your full name:").split()
+print(fname)
+print(lname)
+
+#9
+print("Output for Question 9:.................................................")
+fvariable,svariable=input("Enter Python Programming:").split()
+print(fvariable)
+print(svariable)
+
+#10
+print("Output for Question 10:.................................................")
+a,b,c=input("Enter three words:").split()
+print(a)
+print(b)
+print(c)
+
+#11
+print("Output for Question 11:.................................................")
+a="25"
+a=int(a)
+print(a,type(a))
+
+#12
+print("Output for Question 12:.................................................")
+a="25.5"
+a=float(a)
+print(a,type(a))
+
+#13
+print("Output for Question 13:.................................................")
+a=100
+a=str(a)
+print(a,type(a))
+
+#14
+print("Output for Question 14:.................................................")
+num=int(input("Enter your num"))
+num=str(num)
+print(num,type(num))
+
+#15
+print("Output for Question 15:.................................................")
+num=float(input("Enter your num"))
+num=str(num)
+print(num,type(num))
+
+#16
+print("Output for Question 16:.................................................")
+fname=input("Enter your first name:")
+lname=input("Enter your last name:")
+print(fname+lname)
+print("beacause python takes input value by default in string")
+
+#17
+print("Output for Question 17:.................................................")
+num1=(input("Enter your num1"))
+num2=(input("Enter your num2"))
+num1=int(num1)
+num2=int(num2)
+print(num1+num2)
+
+#18
+print("Output for Question 18:.................................................")
+name = "Rahul"
+age = 20
+print(f"My name is {name} and I am {age} years old.")
+
+#19
+print("Output for Question 19:.................................................")
+a = 10
+b = 20
+print(f"sum is{a+b}")
+
+#20
+print("Output for Question 20:.................................................")
+user_name=input("Enter your namr:")
+user_age=int(input("Enter your age"))
+print(f"My name is {user_name} and I am {user_age} years old.")
+
+#21
+print("Output for Question 21:.................................................")
+product_price=float(input("Enter product price:"))
+print(f"{product_price:.2f}")
+
+#23
+print("Output for Question 23:.................................................")
+product_name=input("Enter product name:")
+product_price=float(input("Enter product price:"))
+product_quantity=int(input("Enter product quantity:"))
+print(f"my favourite product is {product_name} and i will buy {product_quantity} paying {product_price} of each")
+
+#24
+print("Output for Question 24:.................................................")
+print("A", "B", "C")
+
+#25
+print("Output for Question 25:.................................................")
+print("2026", "08", "19",sep="-")
+
+#26
+print("Output for Question 26:.................................................")
+print("Hello",end=" ")
+print("World")
+
+#27
+print("Output for Question 27:.................................................")
+num1=(input("Enter your num1"))
+num2=(input("Enter your num2"))
+print(f"sum {num1+num2}")
+
+#28
+print("Output for Question 28:.................................................")
+product_price=float(input("Enter product price:"))
+product_quantity=int(input("Enter product quantity:"))
+print(f"total cost {product_price*product_quantity}")
+
+#29
+print("Output for Question 29:.................................................")
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+marks=float(input("Enter your marks:"))
+print(f"my name is {name} and i am {age} years old and i will get {marks}% marks in class 10")
+
+#30
+print("Output for Question 30:.................................................")
+student_name=input("Enter student name:")
+student_age=int(input("Enter student age:"))
+student_height=float(input("Enter student height:"))
+student_city=input("Enter student city:")
+print(f"My name is {student_name} and My age is {age} and my height is {height:.2f} and i am from {city}")
