703e99630d056a265c31a3ce5724a4343b0bf777
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-09-04 18:33:28 +0530

    assignment uploaded

---
+++ 06-Boolean and Logical Expressions/code.py
@@ -0,0 +1,149 @@
+#Q=2
+print("Output for the question-2..................................................................")
+print(type(True))
+
+#Q=3
+print("Output for the question-3..................................................................")
+print(type(False))
+
+#Q=4
+print("Output for the question-4..................................................................")
+a=True
+b="True"
+print(type(a),type(b))
+
+#Q=6
+print("Output for the question-6..................................................................")
+a=(10==10)
+print(bool(a))
+
+#Q=7
+print("Output for the question-7..................................................................")
+print(10!=5)
+
+#Q=8
+print("Output for the question-8..................................................................")
+print(5 > 10)
+
+#Q=9
+print("Output for the question-9..................................................................")
+print(5 < 10)
+
+#Q=10
+print("Output for the question-10..................................................................")
+print(18 >= 18)
+
+#Q=11
+print("Output for the question-11..................................................................")
+print(18 <= 18)
+
+#Q=13
+print("Output for the question-13.................................................................")
+print(19==19)
+print(19!=19)
+print(12>11)
+print(12<13)
+print(12>=12)
+print(12<=13)
+
+
+#Q=14
+print("Output for the question-14..................................................................")
+print(type(10 + 5))
+print(type(10 > 5))
+print(type("Python"))
+print(type(10 == 20))
+
+
+#Q=15
+print("Output for the question-15..................................................................")
+age = 20
+print(age >= 18)
+
+#Q=16
+print("Output for the question-16..................................................................")
+marks = 45
+print(marks == 50)
+
+#Q=17
+print("Output for the question-17..................................................................")
+age = 20
+print(age >= 18)
+
+
+#Q=18
+print("Output for the question-18..................................................................")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+#Q=19
+print("Output for the question-19..................................................................")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+#Q=20
+print("Output for the question-20..................................................................")
+print(not True)
+print(not False)
+
+#Q=22
+print("Output for the question-22..................................................................")
+age = 25
+print(age >= 18 and age <= 60)
+
+#Q=23
+print("Output for the question-23..................................................................")
+age = 16
+print(age < 18 or age > 60)
+
+#Q=24
+print("Output for the question-24..................................................................")
+age = 20
+print(not age < 18)
+
+#Q=25
+print("Output for the question-25..................................................................")
+age=18
+print(age>10 and age<50)
+
+#Q=26
+print("Output for the question-26..................................................................")
+age=18
+print(age<10 or age>100)
+
+#Q=27
+print("Output for the question-27..................................................................")
+age=18
+print(not age<18)
+
+#Q=28
+print("Output for the question-28..................................................................")
+print(bool(0))
+print(bool(1))
+print(bool(-5))
+print(bool(""))
+print(bool("Python"))
+print(bool(False))
+print(bool(True))
+print(bool(None))
+
+#Q=29
+print("Output for the question-29..................................................................")
+print(bool(0))
+print(bool(10))
+print(bool(""))
+print(bool("Hello"))
+print(bool(None))
+
+#Q=30
+print("Output for the question-30..................................................................")
+print(bool(0),type(0))
+print(bool(1),type(1))
+print(bool(""),type(""))
+print(bool("Python"),type("Python"))
+print(bool(False),type(False))
+print(bool(None),type(None))
