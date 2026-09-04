4750985174a495b51f3cdb1753699d68cdc503f6
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-09-05 00:14:32 +0530

    assignment uploaded

---
+++ 08.Conditional Statements/code.py
@@ -0,0 +1,202 @@
+#Practice Problems
+#1
+print("Output for Question 1:.................................................")
+num=15
+if num>10:
+    print("Greater than 10")
+
+#2
+print("Output for Question 2:.................................................")    
+age=19
+if age>=18:
+    print("Adult")
+
+#3
+print("Output for Question 3:.................................................")
+num=float(input("Enter number:"))
+if num>0:
+    print("Number isPositive")
+
+#4
+print("Output for Question 4:.................................................")
+marks=40
+if marks>=40:
+    print("Pass")    
+
+#5
+print("Output for Question 5:.................................................")
+num=int(input("Enter number:"))
+if num==0:
+    print("Zero")
+
+#6
+print("Output for Question 6:.................................................")
+num=1
+if num>=0:
+    print("Number is Positive")
+else:
+    print("Number is Not Positive")    
+
+#7
+print("Output for Question 7:.................................................")
+user_age=int(input("Enter your age:")) 
+if user_age>=18:
+    print("Adult")
+else:
+    print("Minor")
+
+#8
+print("Output for Question 8:.................................................")
+a=int(input("Enter number:"))
+if a%2==0:
+    print("Number is Even")
+else:
+    print("Number is Odd")  
+
+#9      
+print("Output for Question 9:.................................................")
+marks=int(input("Enter your marks:"))
+if marks>=40:
+    print("Pass")
+else:
+    print("Fail")
+
+#10
+print("Output for Question 10:.................................................")
+a=int(input("Enter number:"))
+b=int(input("Enter number:"))
+if a>b:
+    print("a is greater than b")        
+else:
+    print("b is greater than a") 
+
+#11
+print("Output for Question 11:.................................................")
+marks=int(input("Enter your marks:"))
+if marks>=90:
+    print("A")
+elif 75<=marks<=89:
+    print("B")
+elif 60<=marks<=74:
+    print("C")
+elif 40<=marks<=59:
+    print("D") 
+else:
+    print("F")
+
+#12
+print("Output for Question 12:.................................................")
+num=int(input("Enter number:"))
+if num>0:
+    print("Number is Positive")
+elif num<0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
+
+#13
+print("Output for Question 13:.................................................")
+a=int(input("enter your choise:"))
+if a==1:
+    print("Monday")
+elif a==2:
+    print("Tuesday")
+elif a==3:
+    print("Wednesday")
+elif a==4:
+    print("Thursday")        
+elif a==5:
+    print("Friday")   
+else:
+    print("Other")   
+
+#14    
+print("Output for Question 14:.................................................")
+marks=int(input("Enter your marks:"))
+if marks>=90:
+    print("Excellent")
+elif 60<=marks<=89:
+    print("Good")
+elif 40<=marks<=59:
+    print("Pass")
+else:
+    print("Fail")
+
+#15
+print("Output for Question 15:.................................................")
+a=int(input("enter your Number:"))
+if a==1:
+    print("1")
+elif a==2:
+    print("2")
+elif a==3:
+    print("3")
+else :
+    print("Other") 
+
+#16
+print("Output for Question 16:.................................................")      
+age=int(input("Enter your age:")) 
+if age>=18:
+    if age<=60:
+        print("Age Between 18 and 60")
+    else:
+        print("Age is greater than 60")        
+else:
+    print("Minor")      
+
+#17
+print("Output for Question 17:.................................................")
+marks=int(input("Enter your marks:"))      
+if marks>=40:
+    if marks>=75:
+        print("Good")
+    else:
+        print("Passed")
+else:
+    print("Failed")
+
+#18
+print("Output for Question 18:.................................................")
+num=int(input("enter your Number:"))
+if num>=0:
+    if num>100:
+        print("Number is greater than 100")
+    else:
+        print("Number is less than 100")          
+else:
+    print("Number is negative")   
+
+#19
+print("Output for Question 19:.................................................")
+age=int(input("Enter your age:")) 
+if age>=18:
+    if age<=60:
+        print("Age Between 18 and 60")
+    else:
+        print("Age is greater than 60")        
+else:
+    print("Minor") 
+
+#20
+print("Output for Question 20:.................................................")
+num=int(input("enter your Number:"))
+if num!=0:
+    if num>0:
+        print("Number is Positive")
+    else:
+        print("Number is Negative")          
+else:
+    print("Number is Zero")   
+
+#21
+print("Output for Question 21:.................................................")
+age=int(input("Enter your age:"))
+marks=int(input("Enter your marks:"))      
+if age>=18:
+    if marks>=40:
+        print("Eligible")
+    else:
+        print("Not Eligible")
+else: 
+    print("Not Eligible")                      
