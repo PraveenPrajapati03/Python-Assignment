c1111888d1af9afe309f920e4cf33dbc7e40b5e3
PraveenPrajapati03 <praveen.prajapati.cg@gmail.com>
2026-09-26 22:44:29 +0530

    assignment uploaded

---
+++ 11.while loop/pattern.py
@@ -0,0 +1,251 @@
+#1
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print("*",end=" ")
+        j+=1
+    i+=1
+    print()
+
+#2
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print(j,end=" ")
+        j+=1
+    i+=1
+    print()
+
+#3
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print(i,end=" ")
+        j+=1
+    i+=1
+    print()
+
+#4
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print("*",end=" ")
+        j+=1
+    i+=1
+    print()
+
+#5
+i=1
+while i<6:
+    j=1
+    while j<=6-i:
+        print("*",end=" ")
+        j+=1
+    i+=1
+    print()
+
+#6
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print(j,end=" ")
+        j+=1
+    i+=1
+    print()    
+
+#7
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print(i,end=" ")
+        j+=1
+    i+=1
+    print()  
+
+#8
+i=1
+while i<6:
+    j=1
+    while j<11:
+        print(i*j,end=" ")
+        j+=1
+    i+=1
+    print()    
+
+#9
+i=1
+while i<4:
+    j=1
+    while j<6:
+        print(i*j,end="  ")
+        j+=1
+    i+=1
+    print()    
+
+#10
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print(j**2,end="")
+        j+=1
+    i+=1
+    print()    
+
+#11
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print(chr(64+j),end=" ")
+        j+=1
+    i+=1
+    print()    
+
+#12
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print(chr(64+i),end=" ")
+        j+=1
+    i+=1
+    print()
+
+#13
+i=1
+while i<6:
+    j=1
+    while j<2*i+1:
+        if j%2!=0:
+            print(j,end=" ")
+        j+=1
+    i+=1
+    print()
+
+#14
+i=1
+while i<6:
+    j=1
+    while j<2*i+1:
+        if j%2==0:
+            print(j,end=" ")
+        j+=1
+    i+=1
+    print()
+
+#15
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print("*",end=" ")
+        j+=1
+    i+=1
+    print()
+
+#16
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print(j,end=" ")
+        j+=1
+    i+=1
+    print()
+
+#17
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print(j+3*(i-1),end=" ")
+        j+=1
+    i+=1
+    print()
+
+#18
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print(j+5*(i-1),end=" ")
+        j+=1
+    i+=1
+    print()
+
+#19
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print(f"({i},{j})",end=" ")
+        j+=1
+    i+=1
+    print()
+
+
+#20
+i=1
+while i<4:
+    j=1
+    while j<4:
+        print(i,j)
+        j+=1
+    i+=1
+
+#21
+i=1
+while i<11:
+    j=1
+    while j<11:
+        print(f"{i}x{j}=>{i*j}",end=" ")
+        j+=1
+    i+=1
+    print()    
+
+#22
+i=1
+while i<6:
+    j=1
+    while j<i+1:
+        print(i,end="")
+        j+=1
+    i+=1
+    print()
+
+#23
+i=1
+while i<6:
+    j=1
+    while j<6-i+1:
+        print(j,end="")
+        j+=1
+    i+=1
+    print()
+
+#24
+i=1
+while i<6:
+    j=1
+    while j<6-i+1:
+        print(6-j,end="")
+        j+=1
+    i+=1
+    print()
+
+#25
+i=1
+while i<6:
+    j=1
+    while j<6:
+        print(i,end="")
+        j+=1
+    i+=1
+    print()
